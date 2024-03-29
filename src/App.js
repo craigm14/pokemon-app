aaca36214b57b16bac341f98d7e63b21578fd6aa
Craig Moore <craig-moore22@outlook.com>
2024-03-29 13:14:44 +0000

    html / tailwind styles added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "./index.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="title bg-regal-blue text-white flex justify-center flex-col items-center text-3xl p-4">
+        <h1>Pokemon React App</h1>
+        <input
+          type="text"
+          className="mt-5 w-[200px] h-[40px] text-sm px-4 py-2 bg-input-field rounded-3xl border-solid border-black border-2 outline-none"
+        />
+      </div>
     </div>
   );
 }
