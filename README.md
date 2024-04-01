f850bb23a4b7c385020374d36caf749f982b40e4
Craig Moore <craig-moore22@outlook.com>
2024-04-01 12:40:11 +0100

    added functionality to search for a pokemon and display img and stats

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Pokemon app
 search for pokemon
 see stats like HP (health points)
 Image
+Attack / Defence 
+Type of Pokemon
+
+Functionality to be added in future 
+
+Favourite a pokemon
--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import axios from "axios";
 
 function App() {
   const [pokemonName, setPokemonName] = useState("");
-
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defence: "",
+    type: "",
+  });
   const searchPokemon = () => {
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
       .then((responce) => {
-        console.log(responce);
+        setPokemon({
+          name: pokemonName,
+          species: responce.data.species.name,
+          img: responce.data.sprites.front_default,
+          hp: responce.data.stats[0].base_stat,
+          attack: responce.data.stats[1].base_stat,
+          defence: responce.data.stats[2].base_stat,
+          type: responce.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
       });
   };
 
@@ -23,7 +41,7 @@ function App() {
             setPokemonName(event.target.value);
           }}
           type="text"
-          className="mt-5 w-[200px] h-[40px] text-sm px-4 py-2 text-black bg-input-field rounded-3xl border-solid border-black border-2 outline-none"
+          className="mt-5 w-[200px] h-[40px] lowercase text-sm px-4 py-2 text-black bg-input-field rounded-3xl border-solid border-black border-2 outline-none"
         />
         <button
           onClick={searchPokemon}
@@ -31,6 +49,21 @@ function App() {
         >
           Search
         </button>
+      </div>
+      <div className="flex justify-center flex-col items-center text-3xl pt-5">
+        {!pokemonChosen ? (
+          <h1>Please chose a pokemon</h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img className="w-[150px]" src={pokemon.img} alt="Pokemon" />
+            <h3>Health Points: {pokemon.hp}</h3>
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Attack: {pokemon.attack}</h3>
+            <h3>Defence: {pokemon.defence}</h3>
+            <h3>Type: {pokemon.type}</h3>
+          </>
+        )}
       </div>
     </div>
   );
